245faf32b03d218b8d5f5f44a35d766f9ec4d12f
ankitsingh <ankit@ost.com>
2020-01-03 19:09:28 +0530

    bare metal server.

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const express = require('express'),
+  router = express.Router(),
+  cookieParser = require('cookie-parser');
+
+module.exports = router;
---
+++ set_env_vars.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Core ENV Details
+export PR_ENVIRONMENT='development'
+export PR_PORT=3001
+export PR_COOKIE_DOMAIN='localhost'
+export PR_APP_NAME='cybrilla'
+export PR_COOKIE_SECRET='aa5298d3a3fe181a3a52d085ee1525df5asa498337f8f3b76ca7df0a5de3211b'
